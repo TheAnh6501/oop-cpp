ce07c03f60af21a22082579a84c48bd6d204dac9
TheAnh6501 <tta6501.work@gmail.com>
2024-08-07 16:57:07 +0700

    Update again (Vẫn có lỗi)

--- OOP_008_classSinhVien/OOP_008_classSinhVien.cpp
+++ OOP_008_classSinhVien/OOP_008_classSinhVien.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 class SinhVien{
-    public:
+    private:
         string hoTen;
         float diemToan;
         float diemAnh;
         float diemLTC;
         float diemCong;
-    
+    public:
         SinhVien(string = "", float = 0, float = 0, float = 0, float = 0);
         void nhap();
         void xuat();
         float tinhDiemTrungBinh();
         float tinhDiemHocBong();
+        bool thiLai(float = 0);
+        friend void sapXep(SinhVien a[], int);
 };
 
 SinhVien::SinhVien(string ht, float dt, float da, float dltc, float dcong){
@@ -52,47 +54,71 @@ float SinhVien::tinhDiemHocBong(){
     return tinhDiemTrungBinh() + diemCong;
 }
 
+bool SinhVien::thiLai(float x){
+    if (x == 0){
+        return (diemToan < 5 || diemAnh < 5 || diemLTC < 5);
+    }
+    if (x == 1){
+        return (diemToan < 5);
+    }
+    if (x == 2){
+        return (diemAnh < 5);
+    } 
+
+    return (diemLTC < 5);
+}
+
+void sapXep(SinhVien a[], int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = i + 1; j < n; j++){
+            if (a[i].tinhDiemHocBong() > a[j].tinhDiemHocBong()){
+                SinhVien temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
 int main(){
     SinhVien a[1000];
     int n, i;
     cout << "So sinh vien: ";
     cin >> n;
-    // In thông tin các sinh viên
     for (i = 0; i < n; i++){
         cout << "Sinh vien thu " << i + 1 << ": ";
         a[i].nhap();
     }
     // Danh sach sinh vien thi lai theo tung mon
-    cout << "Danh sach sinh vien thi lai mon Toan: " << endl;
-    for (i = 0; i < n; i++){
-        if (a[i].diemToan <= 4.0){
-            a[i].xuat();
-        }
-    }
-    cout << "Danh sach sinh vien thi lai mon Anh: " << endl;
-    for (i = 0; i < n; i++){
-        if (a[i].diemAnh <= 4.0){
-            a[i].xuat();
-        }
-    }
-    cout << "Danh sach sinh vien thi lai mon LTC: " << endl;
-    for (i = 0; i < n; i++){
-        if (a[i].diemLTC <= 4.0){
-            a[i].xuat();
-        }
-    }
+    // cout << "Danh sach sinh vien thi lai mon Toan: " << endl;
+    // for (i = 0; i < n; i++){
+    //     if (a[i].diemToan <= 4.0){
+    //         a[i].xuat();
+    //     }
+    // }
+    // cout << "Danh sach sinh vien thi lai mon Anh: " << endl;
+    // for (i = 0; i < n; i++){
+    //     if (a[i].diemAnh <= 4.0){
+    //         a[i].xuat();
+    //     }
+    // }
+    // cout << "Danh sach sinh vien thi lai mon LTC: " << endl;
+    // for (i = 0; i < n; i++){
+    //     if (a[i].diemLTC <= 4.0){
+    //         a[i].xuat();
+    //     }
+    // }
     // Sap xep va in sinh vien theo thu tu dtb
-    for (i = 0; i < n - 1; i++){
-        for (int j = i + 1; j < n; j++){
-            if (a[i].tinhDiemTrungBinh() < a[j].tinhDiemTrungBinh()){
-                swap(a[i], a[j]);
-            }
-        }
-    }
-    cout << "Danh sach sinh vien theo thu tu dtb: " << endl;
-    for (i = 0; i < n; i++){
-        a[i].xuat();
-    }
+    // for (i = 0; i < n - 1; i++){
+    //     for (int j = i + 1; j < n; j++){
+    //         if (a[i].tinhDiemTrungBinh() < a[j].tinhDiemTrungBinh()){
+    //             swap(a[i], a[j]);
+    //         }
+    //     }
+    // }
+    // cout << "Danh sach sinh vien theo thu tu dtb: " << endl;
+    // for (i = 0; i < n; i++){
+    //     a[i].xuat();
+    // }
     // In ra danh sach sinh vien duoc hoc bong
             
     return 0;
